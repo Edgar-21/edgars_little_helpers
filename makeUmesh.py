99b3a16332d14b3ae1575166bd809c286eede868
Edgar <ejpflug@gmail.com>
2024-04-23 09:45:09 -0500

    update docstring to match parameters

--- makeUmesh.py
+++ makeUmesh.py
@@ -7,12 +7,20 @@ import inspect
 # then heal -> autoheal and let that try to fix it, worked for me
 # setting reheal to true will automate this
 
-def makeUmesh(inName, reheal=False):
+def makeUmesh(inName, ratio = 100, angle = 5, reheal=False):
 	"""
-	attempts to make mesh 1 element thick
-	inName : str of .step file
-	outName : str desired name of .cub file
-	size : int suggestion for cubit
+	Attempts to mesh step file using coarse mesh settings for the surface,
+	and tetmeshing the interior. This is particularly for thin volumes
+	in which large, flat elements are desired.
+
+	Export to exodus then attempts to mbconvert it to h5m.
+
+	Deletes the exodus file automatically so watch out if you don't want that
+
+	Arguments:
+		inName (str): path to step file
+		ratio (float): cubit coarse mesh setting
+		angle (float): cubit coarse mesh setting
 	"""
 	# Retrieve Cubit module directory
 	cubit_dir = os.path.dirname(inspect.getfile(cubit))
@@ -38,7 +46,7 @@ def makeUmesh(inName, reheal=False):
 		cubit.cmd('healer autoheal volume 1 rebuild')
 		cubit.cmd('compress')
 
-	cubit.cmd('set trimesher coarse on ratio 100 angle 5')
+	cubit.cmd(f'set trimesher coarse on ratio {ratio} angle {angle}')
 	cubit.cmd('surface all scheme trimesh')
 	
 	#set meshing scheme and mesh
